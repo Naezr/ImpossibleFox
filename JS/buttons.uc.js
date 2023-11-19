83385eeeb00b133e18fd0732239b1d1a36953e87
Naezr <95460152+Naezr@users.noreply.github.com>
2023-11-19 12:23:50 +0700

    Some improvements
    The downloads button is now unscripted and can be moved around in the standard interface. However, the menu and extension buttons fell down a bit, I had to raise them via css.

--- JS/buttons.uc.js
+++ JS/buttons.uc.js
@@ -6,11 +6,9 @@
 	let menub = document.getElementById("PanelUI-menu-button");
 	let ext = document.getElementById("unified-extensions-button");
 	let tabstlbr = document.getElementById("tabbrowser-tabs");
-	let dwnlds = document.getElementById("downloads-button");
         toolbar._customizationTarget.prepend(back);
         back.after(fwd);
-	tabstlbr.after(dwnlds);
-	dwnlds.after(ext);
+	toolbar._customizationTarget.after(ext);
 	ext.after(menub);
     }
     if (gBrowserInit.delayedStartupFinished) init();
